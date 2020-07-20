e588690a2c94478f0d85b41bf433b7f88e4f2eba
Alexander Babai <aliaksandr.babai@gmail.com>
2020-07-21 02:01:53 +0300

    docs(consumer): update root pkg

--- consumer/doc.go
+++ consumer/doc.go
@@ -1,6 +1,6 @@
 /*
 Package consumer contains different types of consumers.
 
-Unlike most other functional interfaces, consumers are expected to operate via side-effects.
+Unlike most other functional types, consumers are expected to operate via side-effects.
 */
 package consumer
