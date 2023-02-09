a96352cfdd930bddc28e7c3a878a62ce06f89561
Aliaksandr Babai <aliaksandr.babai@gmail.com>
2023-02-09 15:05:52 +0300

    docs: fix lint

--- doc.go
+++ doc.go
@@ -1,7 +1,2 @@
-/*
-Package fun contains functional types with useful helper methods.
-
-
-For more information, please visit https://github.com/alebabai/fun
-*/
+// Package fun contains functional types with useful helper methods.
 package fun
